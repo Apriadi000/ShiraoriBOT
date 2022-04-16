67eb700a18072a9fc955560fb813703a16ccc7c2
Apriadi000 <103306472+Apriadi000@users.noreply.github.com>
2022-04-16 23:51:10 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ https://saweria.co/ilmanhdyt
-├ Telkomsel:081351047727
+├ Dana: 085954944195
+├ XL:085954944195
 └────
-`.trim(), '© SHIRAORI', 'Pemilik Bot', '.owner', m)
+`.trim(), '© ＼⋆⃟K⋆⃟A𓆩ᵇᵒᵗ𓆪⋆⃟T ⋆⃟E／', 'Pemilik Bot', '.owner', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
